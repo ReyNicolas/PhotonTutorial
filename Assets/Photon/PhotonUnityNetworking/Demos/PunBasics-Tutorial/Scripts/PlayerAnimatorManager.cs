5e7f114864c95f2221c6af9968e2d5dc3ce4b434
Nicolas <rey.nicolas.92@gmail.com>
2024-02-12 00:12:27 -0300

    Completed Tutorial

--- Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/PlayerAnimatorManager.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/PlayerAnimatorManager.cs
@@ -12,70 +12,100 @@ using UnityEngine;
 
 namespace Photon.Pun.Demo.PunBasics
 {
-	public class PlayerAnimatorManager : MonoBehaviourPun 
+	public class PlayerAnimatorManager : MonoBehaviourPun
 	{
-        #region Private Fields
+		[SerializeField]Animator animator;
 
-        [SerializeField]
-	    private float directionDampTime = 0.25f;
-        Animator animator;
+        [SerializeField]float directionDampTime = 0.25f;
 
-		#endregion
+        private void Update()
+        {
+            if (photonView.IsMine == false && PhotonNetwork.IsConnected == true) return;
 
-		#region MonoBehaviour CallBacks
+            if (!animator)return;
 
-		/// <summary>
-		/// MonoBehaviour method called on GameObject by Unity during initialization phase.
-		/// </summary>
-	    void Start () 
-	    {
-	        animator = GetComponent<Animator>();
-	    }
-	        
-		/// <summary>
-		/// MonoBehaviour method called on GameObject by Unity on every frame.
-		/// </summary>
-	    void Update () 
-	    {
-
-			// Prevent control is connected to Photon and represent the localPlayer
-	        if( photonView.IsMine == false && PhotonNetwork.IsConnected == true )
-	        {
-	            return;
-	        }
-
-			// failSafe is missing Animator component on GameObject
-	        if (!animator)
-	        {
-				return;
-			}
-
-			// deal with Jumping
-            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);			
-
-			// only allow jumping if we are running.
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
             if (stateInfo.IsName("Base Layer.Run"))
             {
-				// When using trigger parameter
-                if (Input.GetButtonDown("Fire2")) animator.SetTrigger("Jump"); 
-			}
-           
-			// deal with movement
+                // When using trigger parameter
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    animator.SetTrigger("Jump");
+                }
+            }
+
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
 
-			// prevent negative Speed.
-            if( v < 0 )
-            {
-                v = 0;
-            }
+            if (v < 0) v = 0;
 
-			// set the Animator Parameters
-            animator.SetFloat( "Speed", h*h+v*v );
-            animator.SetFloat( "Direction", h, directionDampTime, Time.deltaTime );
-	    }
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
 
-		#endregion
+        }
 
-	}
+        //      #region Private Fields
+
+        //      [SerializeField]
+        //   private float directionDampTime = 0.25f;
+        //      Animator animator;
+
+        //#endregion
+
+        //#region MonoBehaviour CallBacks
+
+        ///// <summary>
+        ///// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        ///// </summary>
+        //   void Start () 
+        //   {
+        //       animator = GetComponent<Animator>();
+        //   }
+
+        ///// <summary>
+        ///// MonoBehaviour method called on GameObject by Unity on every frame.
+        ///// </summary>
+        //   void Update () 
+        //   {
+
+        //	// Prevent control is connected to Photon and represent the localPlayer
+        //       if( photonView.IsMine == false && PhotonNetwork.IsConnected == true )
+        //       {
+        //           return;
+        //       }
+
+        //	// failSafe is missing Animator component on GameObject
+        //       if (!animator)
+        //       {
+        //		return;
+        //	}
+
+        //	// deal with Jumping
+        //          AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);			
+
+        //	// only allow jumping if we are running.
+        //          if (stateInfo.IsName("Base Layer.Run"))
+        //          {
+        //		// When using trigger parameter
+        //              if (Input.GetButtonDown("Fire2")) animator.SetTrigger("Jump"); 
+        //	}
+
+        //	// deal with movement
+        //          float h = Input.GetAxis("Horizontal");
+        //          float v = Input.GetAxis("Vertical");
+
+        //	// prevent negative Speed.
+        //          if( v < 0 )
+        //          {
+        //              v = 0;
+        //          }
+
+        //	// set the Animator Parameters
+        //          animator.SetFloat( "Speed", h*h+v*v );
+        //          animator.SetFloat( "Direction", h, directionDampTime, Time.deltaTime );
+        //   }
+
+        //#endregion
+
+    }
 }
